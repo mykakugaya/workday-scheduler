6aa67b88a9dd195d1d73cffe81fd53f46b900c70
Myka Kugaya <mykakug129@gmail.com>
2020-04-29 18:09:07 -0700

    basic page layout done functions working

---
+++ script.js
@@ -0,0 +1,61 @@
+var myDay = [
+    {time: "9AM",
+    event: ""},
+    {time: "10AM",
+    event: ""},
+    {time: "11AM",
+    event: ""},
+    {time: "12PM",
+    event: ""},
+    {time: "1PM",
+    event: ""},
+    {time: "2PM",
+    event: ""},
+    {time: "3PM",
+    event: ""},
+    {time: "4PM",
+    event: ""},
+    {time: "5PM",
+    event: ""}
+];
+
+localStorage.setItem("myDay", myDay);
+
+for(let i = 0; i < myDay.length; i++) {
+    //Add row to schedule
+    var nextRow = $("<div>");
+    nextRow.addClass("row");
+    nextRow.attr("id", myDay[i].time);
+    $("#schedule").append(nextRow);
+
+    //Add time to row
+    var newTime = $("<div>");
+    newTime.text(myDay[i].time);
+    newTime.addClass("col-xs-2 col-sm-2 col-md-2 col-lg-2 hour");
+    nextRow.append(newTime);
+
+    //Add event
+    var newEvent = $("<input>");
+    newEvent.text(myDay[i].event);
+    newEvent.addClass("col-xs-8 col-sm-8 col-md-8 col-lg-8 input");
+    newEvent.attr("data-name", myDay[i].time);
+    nextRow.append(newEvent);
+
+    //Add button
+    var saveBtn = $("<button>");
+    saveBtn.text("Save");
+    saveBtn.addClass("col-xs-2 col-sm-2 col-md-2 col-lg-2 btn btn-primary saveBtn");
+    saveBtn.attr("type", "submit");
+    saveBtn.attr("data-name", i);
+    nextRow.append(saveBtn);  
+}
+
+//Click button to save event
+$(".input").on("keyup", function() {
+    var inputText = this.value;
+    $(".saveBtn").on("click", function(){
+        event.preventDefault();
+        var index = $(this).attr("data-name");
+        myDay[index].event = inputText;
+    });
+})
